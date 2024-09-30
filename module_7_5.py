441fd59ae8e408eb747c6dd11baece7eaf11c448
SergeyL88 <Qwerty@3112311>
2024-09-30 14:31:15 +0400

    Файлы в операционной системе

--- module_7_5.py
+++ module_7_5.py
@@ -11,6 +11,6 @@ for root, dirs, files in os.walk(directory):
         filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime(
             "%d.%m.%Y %H:%M", time.localtime(filetime))
-        parent_dir = root
+        parent_dir = os.path.dirname(filepath)
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {
               filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
